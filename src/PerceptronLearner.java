ac8d9ef1c217ea60ca66f7deee6160adbb3e8936
Yvo Elling <Yvo.elling@live.nl>
2020-03-04 22:50:19 +0100

    Implemented base setup

--- src/PerceptronLearner.java
+++ src/PerceptronLearner.java
@@ -8,6 +8,23 @@ public class PerceptronLearner {
      */
     public String execute(List<PVector> positive, List<PVector> negative, Boolean bias, Integer maxIterations, List<PVector> queries)
     {
+        int iterations = 0;
+        while ( iterations < maxIterations) {
+            
+            //Iterate over all datapoints in @positive
+            for (PVector coordinate : positive) {
+                
+            }
+            
+            //Iterate over all datapoints in @negative
+            for (PVector coordinate : negative) {
+                
+            }
+            
+            //new iteration passed
+            iterations++;
+        }
+        
         //implementation here
         
         return "your output";
